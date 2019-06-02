02209cb24b0189d3eee92676a5a61562524d35a5
sglogowski <s.glogowski@outlook.com>
2019-06-02 16:59:47 +0200

    implementing PerformanceCounter example(NumberOfItems32/RateOfCountsPerSecond32/AverageBase-AverageTimer32).

---
+++ performance-counters/PerformanceCounterExample.cs
@@ -0,0 +1,124 @@
+﻿using System.Diagnostics;
+
+namespace DotNetExamples.PerformanceCounters
+{
+    public class PerformanceCounterExample
+    {
+        const string PerfCounterCategoryName = "DotNetExamples.PerformanceCounters.PerformanceCounterExample";
+
+        const string PerfCounterTotalOperationsExecutedCounterName = "total operations executed";
+        const string PerfCounterOperationsPerSecondCounterName = "operations per second";
+        const string PerfCounterAverageTimePerOperationCounterName = "average time per operation";
+
+        public static void Init()
+        {
+            // Check if existing performance counter category definition for given category name.
+            var exists = PerformanceCounterCategory.Exists(PerfCounterCategoryName);
+            if (exists)
+            {
+                // Deleting performance counter category definitions for given category name.
+                PerformanceCounterCategory.Delete(PerfCounterCategoryName);
+            }
+
+            var counters = new CounterCreationDataCollection
+            {
+                new CounterCreationData()
+                {
+                    CounterName = PerfCounterTotalOperationsExecutedCounterName,
+                    CounterHelp = "Total number of operations executed",
+                    CounterType = PerformanceCounterType.NumberOfItems32
+                    // How many times occured since create.
+                },
+
+                new CounterCreationData()
+                {
+                    CounterName = PerfCounterOperationsPerSecondCounterName,
+                    CounterHelp = "Number of operations executed per second",
+                    CounterType = PerformanceCounterType.RateOfCountsPerSecond32
+                    // How many times occured per second.
+                },
+
+                new CounterCreationData()
+                {
+                    CounterName = PerfCounterAverageTimePerOperationCounterName,
+                    CounterHelp = "Average duration per operation execution",
+                    CounterType = PerformanceCounterType.AverageTimer32
+                    // How long the average operations last.
+                },
+
+                new CounterCreationData()
+                {
+                    CounterName = $"{ PerfCounterAverageTimePerOperationCounterName } base",
+                    CounterHelp = "Average duration per operation execution base",
+                    CounterType = PerformanceCounterType.AverageBase
+                    // Required for PerfCounterAverageTimePerOperationCounterName calculation, increament after AverageTimer32 
+                }
+            };
+
+            // Creating performance counter category definitions for given category name with given defined counters.
+            PerformanceCounterCategory.Create(
+                categoryName: PerfCounterCategoryName,
+                categoryHelp: "Sample category for Codeproject",
+                categoryType: PerformanceCounterCategoryType.Unknown,
+                counterData: counters
+            );
+        }
+
+        /// <summary>
+        /// Increment TotalOperationsExecuted performance counter.
+        /// </summary>
+        /// <param name="value">How many times operations occurred</param>
+        public static void IncrementOperationsExecutedCounter(long value = 1)
+        {
+            var totalOperationsExecuted = new PerformanceCounter
+            {
+                CategoryName = PerfCounterCategoryName,
+                CounterName = PerfCounterTotalOperationsExecutedCounterName,
+                ReadOnly = false
+            };
+
+            totalOperationsExecuted.IncrementBy(value);
+        }
+
+        /// <summary>
+        /// Increment OperationsPerSecond performance counter.
+        /// </summary>
+        /// <param name="value">How many times operations occurred</param>
+        public static void IncrementOperationsPerSecondCounter(long value = 1)
+        {
+            var operationsPerSecond = new PerformanceCounter
+            {
+                CategoryName = PerfCounterCategoryName,
+                CounterName = PerfCounterOperationsPerSecondCounterName,
+                ReadOnly = false
+            };
+
+            operationsPerSecond.IncrementBy(value);
+        }
+
+        /// <summary>
+        /// Increment AverageTimePerOperation performance counter.
+        /// </summary>
+        /// <param name="ellapsedTicks">How long all operations take, eg. Stopwatch.EllapsedTicks</param>
+        /// <param name="value">How many times operations occurred</param>
+        public static void IncrementAverageTimePerOperationCounter(long ellapsedTicks = 1, long value = 1)
+        {
+            var averageTimePerOperation = new PerformanceCounter
+            {
+                CategoryName = PerfCounterCategoryName,
+                CounterName = PerfCounterAverageTimePerOperationCounterName,
+                ReadOnly = false
+            };
+
+            var averageTimePerOperationBase = new PerformanceCounter
+            {
+                CategoryName = PerfCounterCategoryName,
+                CounterName = $"{PerfCounterAverageTimePerOperationCounterName} base",
+                ReadOnly = false
+            };
+
+            averageTimePerOperation.IncrementBy(ellapsedTicks);
+            averageTimePerOperationBase.IncrementBy(value);
+        }
+    }
+}
---
+++ performance-counters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DotNetExamples.PerformanceCounters
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //PerformanceCounterExample.Init();
+            var watch = new Stopwatch();
+
+            while (true)
+            {
+                Console.WriteLine("Start processing");
+                watch.Start();
+
+                var operations = 0;
+
+                for (
+                    var pressedKey = ConsoleKey.Escape;
+                    pressedKey != ConsoleKey.Spacebar; 
+                    pressedKey = Console.ReadKey().Key
+                )
+                {
+                    Console.WriteLine($"Not Spacebar, increments operations counter: {++operations}");
+                }
+
+                watch.Stop();
+                Console.WriteLine("Stop processing");
+
+                PerformanceCounterExample.IncrementOperationsExecutedCounter(operations);
+                PerformanceCounterExample.IncrementOperationsPerSecondCounter(operations);
+                PerformanceCounterExample.IncrementAverageTimePerOperationCounter(watch.ElapsedTicks, operations);
+            }
+        }
+    }
+}
